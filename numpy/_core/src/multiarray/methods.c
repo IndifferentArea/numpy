09c9ff565b9615a027d6cd7997f5d5733b8a5aea
makima <alexspace@foxmail.com>
2025-01-13 13:03:13 +0800

    add cleanup

--- numpy/_core/src/multiarray/methods.c
+++ numpy/_core/src/multiarray/methods.c
@@ -1909,6 +1909,7 @@ array_reduce_ex_picklebuffer(PyArrayObject *self, int protocol)
          * just fall back on regular __reduce_ex__ implementation
          * (gh-12745).
          */
+        Py_DECREF(rev_perm);
         PyErr_Clear();
         return array_reduce_ex_regular(self, protocol);
     }
